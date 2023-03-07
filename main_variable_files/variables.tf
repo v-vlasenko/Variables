b9de8a4ac127d4bece863ed83e532f4a79cd5024
DanaRoshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2023-03-07 19:12:44 +0200

    Add files via upload

---
+++ main_variable_files/variables.tf
@@ -0,0 +1,15 @@
+variable "quantity" {
+	default = 2
+	type = number
+}
+variable "prefix" {
+	default = ["Mr", "Mrs", "Sir"]
+	type = list
+}
+variable "run_id" {
+  default = "run_id_2"
+}
+variable "strings" { 
+default = ["a","b","c","d"]
+type = list(string)
+}

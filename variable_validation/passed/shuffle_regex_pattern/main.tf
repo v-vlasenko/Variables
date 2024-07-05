dc003f280b29f62e94b2943fb712458526bc46e8
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-05 11:31:58 +0300

    Create main.tf

---
+++ variable_validation/passed/shuffle_regex_pattern/main.tf
@@ -0,0 +1,14 @@
+resource "random_shuffle" "my_shuffle" {
+  input = var.fruit
+  result_count = length(var.strings)
+}
+variable "fruit" {
+  type        = string
+  description = "What fruit to pick?"
+  default     = "apple"
+
+  validation {
+    condition     = can(regex("^(lemon|apple|mango|banana|cherry)$", var.fruit))
+    error_message = "Invalid fruit selected, only allowed fruits are: 'lemon', 'apple', 'mango', 'banana', 'cherry'. Default 'apple'"
+  }
+}

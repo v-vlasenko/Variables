2e3062f0bc21051202145bbd4439a61e1b258b58
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-05 14:26:24 +0300

    Update main.tf

--- variable_validation/passed/validation_contains/main.tf
+++ variable_validation/passed/validation_contains/main.tf
@@ -6,20 +6,20 @@ resource "random_pet" "pet" {
 variable "prefix" {
   type        = string
   description = "What fruit to pick?"
-  default     = "appl"
+  #default     = "appl"
 
- validation {
+/* validation {
     condition = contains(
       ["lemon", "cherry", "apple", "banana", "mango"],
       var.prefix
     )
     error_message = "Err: prefix is not valid."
-  }
+  }*/
 
 /*validation {
     condition = contains("", var.prefix)
     error_message = "Err: prefix is not valid."
-}
+}*/
 validation {
     condition = var.prefix == ""
     error_message = "Err: prefix is not valid."
@@ -27,6 +27,6 @@ validation {
 validation {
     condition = length(var.prefix) == 0
     error_message = "Err: prefix is not valid."
-}*/
+}
 
 }

e4c62bfea647619f3ef516658097e4b5493b3ef4
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-05 16:17:32 +0300

    Update main.tf

--- variable_validation/passed/length_validation/main.tf
+++ variable_validation/passed/length_validation/main.tf
@@ -1,4 +1,4 @@
-resource "random_pet" "pet" {
+/*resource "random_pet" "pet" {
  prefix = var.prefix
  separator = "."
  length = 5
@@ -16,4 +16,20 @@ validation {
     condition     = can(regex("^(lemon|apple|mango|banana|cherry)$", var.prefix))
     error_message = "Invalid fruit selected, only allowed fruits are: 'lemon', 'apple', 'mango', 'banana', 'cherry'. Default 'apple',Invalid fruit selected, only allowed fruits are: 'lemon', 'apple', 'mango', 'banana', 'cherry'. Default 'apple'"
   }
+}*/
+
+resource "random_password" "password" {
+  count = 1
+  length           = var.long
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
 }
+variable "long" {
+  type        = string
+  description = "What length?"
+  default = "5"
+
+validation {
+    condition     = length(var.long) <= 3
+    error_message = "Err: prefix is too long."
+  }

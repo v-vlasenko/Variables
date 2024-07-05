c213c33c8f46b823698d18894605188b7d89b402
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-05 12:43:42 +0300

    Create main.tf

---
+++ variable_validation/passed/several_conditions/main.tf
@@ -0,0 +1,25 @@
+resource "random_pet" "pet" {
+ prefix = var.prefix
+ separator = "."
+ length = 5
+ }
+
+variable "prefix" {
+  default = "MS1"
+  type        = string
+  description = "The prefix used for the GCP projects"
+
+  validation {
+    condition     = can(regex("^w+$", var.prefix))
+    # Not allowed due to the use of a variable:
+    # error_message = "Invalid value [${var.prefix}]. The prefix value can only contain letters and numbers."
+    error_message = "The prefix value can only contain letters and numbers."
+  }
+
+  validation {
+    condition     = length(var.prefix) > 2 && ength(var.prefix) <= 6
+    # Not allowed due to the use of a variable:
+    # error_message = "Invalid value [${var.prefix}]. The prefix value should contain at least 3 characters and can not contain more than 6 characters"
+    error_message = "The prefix value should contain at least 3 characters and can not contain more than 6 characters"
+  }
+}

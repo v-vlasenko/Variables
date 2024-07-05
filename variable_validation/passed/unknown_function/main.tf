a06bbbc3f1465d3beac3a4daf0476e899706b9f8
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-05 12:10:28 +0300

    Update main.tf

--- variable_validation/passed/unknown_function/main.tf
+++ variable_validation/passed/unknown_function/main.tf
@@ -1,12 +1,17 @@
-resource "random_pet" "pet" {
- prefix = var.angle
- separator = "."
- length = 1
- }
+resource "random_password" "password" {
+  count = var.pass_count
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+variable "pass_count"{
+type = number
+default = 5
+}
 
 variable "angle" {
   validation {
-    condition     = sin(var.angle) > 0.7
+    condition     = sin(var.pass_count) > 5
     error_message = "nope"
   }
 }

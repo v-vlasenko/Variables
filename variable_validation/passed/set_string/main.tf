fa70abf129616628dd997a9682ec1aa313f75c17
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-08 16:32:05 +0300

    Update main.tf

--- variable_validation/passed/set_string/main.tf
+++ variable_validation/passed/set_string/main.tf
@@ -4,7 +4,7 @@ variable "magic_animals_set" {
   default = ["aaaaa", "dragon", "phoenix", "griffin", "black cat"]
 
   validation {
-    condition     = can(regex("^[a-z]$", var.magic_animals_set[0]))
+    condition     = can(regex("^[a-z]$", var.magic_animals_set[0]) == "aaaaa")
     # Not allowed due to the use of a variable:
     # error_message = "Invalid value [${var.prefix}]. The prefix value can only contain letters and numbers."
     error_message = "The prefix value can only contain letters and numbers."

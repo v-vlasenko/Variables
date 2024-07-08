aa93967cb81c7b5c2d4e4b8dee03a18f6a9eb9fd
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-08 15:21:57 +0300

    Create main.tf

---
+++ variable_validation/passed/string/main.tf
@@ -0,0 +1,9 @@
+ variable "string_may_not_contain" {
+  type = string
+  default = "test"
+
+  validation {
+    error_message = "Value cannot contain a \"/\"."
+    condition = !can(regex("/", var.string_may_not_contain))
+  }
+}

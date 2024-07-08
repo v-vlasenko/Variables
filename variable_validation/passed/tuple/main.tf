063eb0cdb1fe4959f51c109451c39aff7f0f1905
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-08 17:04:40 +0300

    Update main.tf

--- variable_validation/passed/tuple/main.tf
+++ variable_validation/passed/tuple/main.tf
@@ -1,9 +1,9 @@
 variable "magic_animal_tuple" {
   type    = tuple([string, string, string])
-  default = ["Usual Centaur", "Big Earth", "Non-Archery-Dry-check-on-222staging"]
+  default = ["UsualCentaur####", "Big Earth", "Non-Archery-Dry-check-on-222staging"]
 
   validation {
-    condition     = can(regex("^(Usual Centaur|Big Earth|Non-Archery-Dry-check-on-222staging)$", var.magic_animal_tuple))
+    condition     = can(regex("^[a-z]+[0-9]+$", var.magic_animal_tuple[0]))
     error_message = "Should be failed!!!!!!Invalid fruit selected, only allowed fruits are: 'lemon', 'apple', 'mango', 'banana', 'cherry'. Default 'apple',Invalid fruit selected, only allowed fruits are: 'lemon', 'apple', 'mango', 'banana', 'cherry'. Default 'apple'"
   }
   }

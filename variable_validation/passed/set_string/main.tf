cb69760956dd3fcc2c8a96895fce48906143975b
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-08 16:42:07 +0300

    Update main.tf

--- variable_validation/passed/set_string/main.tf
+++ variable_validation/passed/set_string/main.tf
@@ -1,10 +1,10 @@
 variable "magic_animals_set" {
   type    = set(string)
   #sensitive = true
-  default = ["53453453.345345aaabbbccc23454", "dragon", "phoenix", "griffin", "black cat"]
+  default = ["unicorn$", "dragon", "phoenix", "griffin", "black cat"]
 
   validation {
-    condition     = regex("[a-z]+", var.magic_animals_set[0]) == "aaabbbccc"
+    condition     = regex("[a-z]+", var.magic_animals_set[0]) == "unicorn"
     # Not allowed due to the use of a variable:
     # error_message = "Invalid value [${var.prefix}]. The prefix value can only contain letters and numbers."
     error_message = "The value can only contain letters and numbers."

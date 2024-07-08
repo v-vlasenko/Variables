bda092ba58bf3834cea493d66ccd7c1bc5900300
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-08 14:35:32 +0300

    Update main.tf

--- variable_validation/passed/complex_var/main.tf
+++ variable_validation/passed/complex_var/main.tf
@@ -4,7 +4,7 @@ variable "magic_animals_list" {
   #default = ["unicorn", "dragon", "phoenix", "griffin", "thestral", "dog"]
 
 validation {
-    condition = alltrue([for m in ["unicorn"] :  m => contains(["unicorn", "dragon", "phoenix", "griffin", "thestral", "dog"], m)])
+    condition = alltrue([for m in ["unicorn"] : m => contains(["unicorn", "dragon", "phoenix", "griffin", "thestral", "dog"], m)])
     error_message = "Some animal is not listed in default values"
   }
 }

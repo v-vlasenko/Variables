111e9913a848f73385f9519b521ff6531fa8f5a4
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-08 18:22:26 +0300

    Update main.tf

--- variable_validation/passed/string2/main.tf
+++ variable_validation/passed/string2/main.tf
@@ -4,7 +4,7 @@ default = "dana10"
 
   validation {
     # formatdate fails if the second argument is not a valid timestamp
-    condition     = can(formatdate("", var.timestamp))
+    condition     = can(formatdate("DD YYYY", var.timestamp))
     error_message = "The timestamp argument requires a valid RFC 3339 timestamp."
   }
 }

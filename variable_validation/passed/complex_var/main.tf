d139a101b615dcd7cc012848e1897cc8eb79c77d
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-08 14:49:29 +0300

    Update main.tf

--- variable_validation/passed/complex_var/main.tf
+++ variable_validation/passed/complex_var/main.tf
@@ -79,17 +79,19 @@ variable "magic_animals_set" {
 
 
 variable "mymap" {
-  type = map(string)
+  type = map(object({
+    my_key = string
+  }))
 
   validation {
     condition = alltrue(
-       [for obj in keys(var.mymap) : can(regex("^key.*",obj))]
+       [for obj in keys(var.mymap) : can(regex("^my.*",obj))]
     )
     error_message = "Each map key should include key string."
   }
 
-  default = {"key1" = "val1"}
-  #default = {"key1" = "val1", "key2" = "val2"}
+  #default = [{"key1" = "val1"}]
+  default = {"my_key1" = "val1", "key2" = "val2"}
   #default = {"okey1" = "val1", "key2" = "val2"}
   #default = {"ok1" = "val1", "key2" = "val2"}
 }

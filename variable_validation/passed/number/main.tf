e557ee454d94e786bc3c79a3085256ac6386cf74
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-08 15:22:18 +0300

    Create main.tf

---
+++ variable_validation/passed/number/main.tf
@@ -0,0 +1,9 @@
+variable "num_in_range" {
+  type        = number
+  default     = 1
+
+  validation {
+    condition     = var.num_in_range >= 1 && var.num_in_range <= 16 && floor(var.num_in_range) == var.num_in_range
+    error_message = "Accepted values: 1-16."
+  }
+}

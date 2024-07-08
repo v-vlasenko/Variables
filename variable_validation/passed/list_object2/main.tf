c60de52059611525a2504467dbf4e3c6d4a898f9
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-08 15:19:41 +0300

    Create main.tf

---
+++ variable_validation/passed/list_object2/main.tf
@@ -0,0 +1,38 @@
+variable "magic_animals" {
+  type = list(object({
+    name       = string
+    power      = string
+    element    = string
+    description = string
+  }))
+  default = [
+    {
+      name        = "unicorn"
+      power       = "sparkle"
+      element     = "light"
+      description = "A mystical creature with a single horn on its forehead."
+    },
+    {
+      name        = "dragon"
+      power       = "fire"
+      element     = "fire"
+      description = "A powerful creature with the ability to breathe fire."
+    },
+    {
+      name        = "phoenix"
+      power       = "rebirth"
+      element     = "fire"
+      description = "A legendary bird that is reborn from its own ashes."
+    },
+    {
+      name        = "griffin"
+      power       = "majesty"
+      element     = "air"
+      description = "A majestic creature with the body of a lion and the head of an eagle."
+    }
+  ]
+  validation {
+    condition = contains(["cat", "pet"], var.magic_animals[0].name)
+    error_message = "Invalid object, name can be unicorn(set by default configuration for first object in the template)."
+  }
+}

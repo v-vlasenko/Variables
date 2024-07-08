6ba3643530ee3f394ab6fb1986286832760771c0
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-08 13:25:45 +0300

    Update main.tf

--- variable_validation/passed/complex_var/main.tf
+++ variable_validation/passed/complex_var/main.tf
@@ -135,6 +135,17 @@ variable "num_in_range" {
   }
 }
 
+
+variable "timestamp" {
+  type        = string
+
+  validation {
+    # formatdate fails if the second argument is not a valid timestamp
+    condition     = can(formatdate("", var.timestamp))
+    error_message = "The timestamp argument requires a valid RFC 3339 timestamp."
+  }
+}
+
   
 
 

8fa6c698385d93ff6901d5917c84d2c5066370ac
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-08 17:55:14 +0300

    Update main.tf

--- variable_validation/passed/map_object/main.tf
+++ variable_validation/passed/map_object/main.tf
@@ -3,15 +3,16 @@ variable "mymap" {
     my_key = string
   }))
 
+  default = {
+log1 = {my_key: "val%"}, 
+log2 = {my_key: "val2"}
+}
+
   validation {
     condition = alltrue(
-       [for obj in values(var.mymap) : regex("val[a-z]+", obj) == "val"]
+       [for value in values(var.mymap) : regex("val[a-z]+", value.my_key) == "val"]
     )
     error_message = "Each map key should include key string."
   }
 
-  #default = [{"key1" = "val1"}]
-  default = [{my_key: "val%"}, {my_key: "val2"}]
-  #default = {"okey1" = "val1", "key2" = "val2"}
-  #default = {"ok1" = "val1", "key2" = "val2"}
 }

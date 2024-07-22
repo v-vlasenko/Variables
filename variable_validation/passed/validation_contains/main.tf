faa6fd84ff01701149d158a4a82a1dab4814b7b6
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-22 16:57:38 +0300

    Update main.tf

--- variable_validation/passed/validation_contains/main.tf
+++ variable_validation/passed/validation_contains/main.tf
@@ -16,15 +16,15 @@ variable "prefix" {
     error_message = "Err: prefix is not valid."
   }*/
 
-/*validation {
-    condition = contains([""], var.prefix)
-    error_message = "Err: prefix is not valid."
-}*/
-
 validation {
-    condition = var.prefix == ""
+    condition = contains("", var.prefix)
     error_message = "Err: prefix is not valid."
 }
+
+/*validation {
+    condition = var.prefix == ""
+    error_message = "Err: prefix is not valid."
+}*/
 /*validation {
     condition = length(var.prefix) == 0
     error_message = "Err: prefix is not valid."

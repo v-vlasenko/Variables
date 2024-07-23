2ea6ca11658108e55f4a7797750b162c1afbad0e
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-23 13:26:30 +0300

    Update main.tf

--- variable_validation/passed/Valid_IPv4_CIDR/main.tf
+++ variable_validation/passed/Valid_IPv4_CIDR/main.tf
@@ -1,6 +1,6 @@
 variable "string_like_valid_ipv4_cidr" {
   type    = string
-  default = "	192.168.100.255/16"
+  default = "192.168.100.255/16"
 
   validation {
     condition     = can(cidrhost(var.string_like_valid_ipv4_cidr, 32))

ec5b85ed34aa53e13059bfadfba5afc25679a77b
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-23 11:51:21 +0300

    Create main.tf

---
+++ variable_validation/passed/string_aws_region/main.tf
@@ -0,0 +1,8 @@
+variable "string_like_aws_region" {
+  type = string
+  default = "us-east-1"
+
+  validation {
+    condition     = can(regex("[a-z][a-z]-[a-z]+-[1-9]", var.string_like_aws_region))
+    error_message = "Must be valid AWS Region names."
+  }

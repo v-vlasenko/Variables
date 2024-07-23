1d0e29aeeb840787ca75b00c3d4cbb4b0af55e93
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-23 16:04:20 +0300

    Create main.tf

---
+++ variable_validation/passed/default_null/main.tf
@@ -0,0 +1,8 @@
+variable "test" {
+  type = string
+  default = null
+  validation {
+    condition = try(contains(["a", "b", "c"], var.test, true))
+    error_message = "test must be one of a, b, c or null"
+  }
+}

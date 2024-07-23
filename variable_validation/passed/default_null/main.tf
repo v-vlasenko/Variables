2324c91465f530c74da1690f541ed6d0d087b99a
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-23 16:06:50 +0300

    Update main.tf

--- variable_validation/passed/default_null/main.tf
+++ variable_validation/passed/default_null/main.tf
@@ -1,8 +1,17 @@
 variable "test" {
   type = string
   default = null
-  validation {
+  /*validation {
     condition = try(contains(["a", "b", "c"], var.test, true))
+    error_message = "test must be one of a, b, c or null"
+  }*/
+
+ validation {
+    condition = (
+      var.test != null ?
+      contains(["a", "b", "c"], var.test) :
+      true
+    )
     error_message = "test must be one of a, b, c or null"
   }
 }

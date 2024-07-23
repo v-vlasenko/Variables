9581cc1a8b0ccd03769375ba8fbd1e6b3ebe8de0
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-07-23 14:17:31 +0300

    Update main.tf

--- variable_validation/passed/upper_case/main.tf
+++ variable_validation/passed/upper_case/main.tf
@@ -1,6 +1,6 @@
 variable "ecr_repo_name" {
   validation {
-    condition     = var.ecr_repo_name != upper(var.ecr_repo_name)
+    condition     = var.ecr_repo_name == upper(var.ecr_repo_name)
     error_message = "ECR repository name cannot be in uppercase"
   }
 }

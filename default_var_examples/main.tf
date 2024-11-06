d58bf96e70ab1abd40035de7aae0c9f58cb34c10
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-11-06 14:02:18 +0200

    Update main.tf

--- default_var_examples/main.tf
+++ default_var_examples/main.tf
@@ -1,25 +1,25 @@
 # example-1-default-value
-variable "var_name" {
+variable "var_name1" {
   type = string
   default  = "default-value"
 }
 # example-2-empty string
-variable "var_name" {
+variable "var_name2" {
   type = string
   default  = ""
 }
 # example-3-string with spaces (best to avoid)
-variable "var_name" {
+variable "var_name3" {
   type = string
   default  = "   "
 }
 # example-4-null value 
-variable "var_name {
+variable "var_name4" {
  type = string
  default = null
 }
 
 #example-5 - no default value
-variable "var_name" {
+variable "var_name5" {
   type = string
 }

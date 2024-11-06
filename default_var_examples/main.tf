26ce5ffc08bb0b9dc891a6ffe1fd5ebb7d5a882b
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-11-06 14:43:54 +0200

    Update main.tf

--- default_var_examples/main.tf
+++ default_var_examples/main.tf
@@ -3,23 +3,48 @@ variable "var_name1" {
   type = string
   default  = "default-value"
 }
+resource "null_resource" "name" {
+  triggers = {
+    trigger = var.var_name1
+  }
+}
 # example-2-empty string
 variable "var_name2" {
   type = string
   default  = ""
+}
+resource "null_resource" "name" {
+  triggers = {
+    trigger = var.var_name2
+  }
 }
 # example-3-string with spaces (best to avoid)
 variable "var_name3" {
   type = string
   default  = "   "
 }
+resource "null_resource" "name" {
+  triggers = {
+    trigger = var.var_name3
+  }
+}
 # example-4-null value 
 variable "var_name4" {
  type = string
  default = null
 }
+resource "null_resource" "name" {
+  triggers = {
+    trigger = var.var_name4
+  }
+}
 
 #example-5 - no default value
 variable "var_name5" {
   type = string
+}
+resource "null_resource" "name" {
+  triggers = {
+    trigger = var.var_name5
+  }
 }

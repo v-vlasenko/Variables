814ea5bddff8d94c216d5001c072b6377ec12919
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-11-08 13:07:09 +0200

    Create main.tf

---
+++ variable_validation/passed/var_without_value_rule/main.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+  type        = string
+
+validation {
+    condition = var.prefix == ""
+    error_message = "variable could be saved without value"
+}
+}
+
+variable "prefix2" {
+  type        = string
+validation {
+    condition = length(var.prefix) == 0
+    error_message = "variable could be saved without value"
+}
